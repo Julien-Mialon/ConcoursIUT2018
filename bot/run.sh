83043c3c348cd5e965174d09137a24d36750b747
Millian Poquet <millian.poquet@inria.fr>
2018-03-31 13:23:22 +0200

    run.sh: parameters & cleanup

--- bot/run.sh
+++ bot/run.sh
@@ -1,8 +1,28 @@
 #!/bin/bash
 
+# Arguments parting
+usage="run.sh [host] [port]"
+case "$#" in
+    0)
+        host='localhost'
+        port='8889'
+        ;;
+    1)
+        host="$1"
+        port='8889'
+        ;;
+    2)
+        host="$1"
+        port="$2"
+        ;;
+esac
+echo "host=${host}"
+echo "port=${port}"
+
+# Bot
 source ./bot.sh
 
-exec 3<>/dev/tcp/localhost/8889
+exec 3<>/dev/tcp/${host}/${port}
 echo -e '{"nickname": "Bash Binder"}' >&3
 
 currentType="init"
@@ -21,4 +41,3 @@ do
             exit ;;
     esac
 done
-#cat <&3
